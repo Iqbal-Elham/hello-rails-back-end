f3868256b51df413e5b047f278d0fd2fb71217f1
iqbal-elham <iqbal.elham7@gmail.com>
2023-07-23 23:50:42 +0430

    Add random_greeting in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'message/index'
+  # get 'message/index'
   get '/random_greeting', to: 'message#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
